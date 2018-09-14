b426ef77f42c09668085d3902d3cc13aa3d08950
raalbright <ray.a.albright@gmail.com>
2018-09-14 10:15:26 -0500

    finished part 2

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const friends = ['Justin', 'Ashley', 'Chris', 'Natilie', 'Aaron'];
+const singBtn = document.querySelector('input[value="Sing!"]');
+const body = document.body;
 
-for (friend of friends) {
-    console.log(friend.toUpperCase() + ':')
-    for (let i = 99; i > 0; i--) {
-        if (i < 2) {
-            console.log(i + ' line of code in the file, ' + i + ' line of code; ' + friend + ' strikes one out, clears it all out, no more lines of code in the file');
-        } else {
-            console.log(i + ' lines of code in the file, ' + i + ' lines of code; ' + friend + ' strikes one out, clears it all out, ' + (i - 1) + ' lines of code in the file');
-        }
-    }
+
+const handleClick= () => {
+    const friends = ['Justin', 'Ashley', 'Chris', 'Natilie', 'Rob'];
+    friends.forEach(sing);
 }
+
+const sing = (name) => {
+    const friendDiv = document.createElement('div');
+    const friendHeader = document.createElement('h3');
+    friendDiv.classList.add('friend');
+    friendHeader.innerText = name.toUpperCase() + ':';
+    friendDiv.appendChild(friendHeader);
+
+
+    for (let i = 99; i > 0; i--) {
+        const para = document.createElement('p');
+        let lyric = '';
+
+        if (i === 2) {
+            lyric = i + ' line of code in the file, ' + i + ' line of code; ' + name +
+            ' strikes one out, clears it all out, no more lines of code in the file.';
+        } else if (i === 1) {
+            lyric = i + ' line of code in the file, ' + i + ' line of code; ' + name +
+                ' strikes one out, clears it all out, no more lines of code in the file.';
+        } else {
+            lyric = i + ' lines of code in the file, ' + i + ' lines of code; ' + name + 
+            ' strikes one out, clears it all out, ' + (i - 1) + ' lines of code in the file.';
+        }
+        para.innerText = lyric;
+        friendDiv.appendChild(para);
+    }
+    body.appendChild(friendDiv);
+}
+
+singBtn.addEventListener('click', handleClick);
